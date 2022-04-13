af4b1d23d4605e2e55a2d0537b91a5a33582cd00
Frankie Xiao <fxiaoX089@live.com>
2022-04-13 17:08:12 -0500

    Update index.html, script.js, and style.css files
    - Deleted some un-needed elements in index.html
    - Added function to create cells and append those cells to a row that is
      appended to a square within the window.
    - CSS styles to styles elements so user can see them.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Create a window to for the sketch, default 16x16
+// Create a square window for where the cells will be create in
+const container = document.querySelector('.container');
+const square = document.createElement('div');
+square.classList.add("square");
+container.appendChild(square);
+
+// Create cells
+// Make a div to hold div cells
+function makeGrid(size){
+    for(let i = 0; i < size; i++){
+        // Make rows
+        const row = document.createElement('div');
+        row.classList.add('row');
+        square.appendChild(row);
+        // Fill in rows with cells
+        for(let i = 0; i < size; i++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+    }
+}
+
+makeGrid(16);
